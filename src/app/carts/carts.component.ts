726f51bc288b5710c6e288e0a98788f1feb0f1ab
rkoren1 <rok.koren@netica.si>
2023-04-02 16:37:54 +0200

    added fontawesome, cart component now loads carts and their data from api, click on cart redirects to user profile

--- src/app/carts/carts.component.ts
+++ src/app/carts/carts.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CartsService } from "./carts.service";
+import { Cart } from "app/shared/interfaces/cart.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-carts',
-  templateUrl: './carts.component.html',
-  styleUrls: ['./carts.component.scss'],
+  selector: "app-carts",
+  templateUrl: "./carts.component.html",
+  styleUrls: ["./carts.component.scss"],
 })
 export class CartsComponent implements OnInit {
-  constructor() {}
+  cartsData: Cart[];
+  constructor(private cartsService: CartsService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cartsService.getCarts().subscribe((carts) => (this.cartsData = carts));
+  }
+
+  cartClicked(userId: number) {
+    this.router.navigate(["/user-profile/", userId]);
+  }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 declare const $: any;
 declare interface RouteInfo {
@@ -8,25 +8,25 @@ declare interface RouteInfo {
   class: string;
 }
 export const ROUTES: RouteInfo[] = [
-  { path: '/users', title: 'Users', icon: 'dashboard', class: '' },
+  { path: "/users", title: "Users", icon: "perm_identity", class: "" },
   {
-    path: '/carts',
-    title: 'Carts',
-    icon: 'content_paste',
-    class: '',
+    path: "/carts",
+    title: "Carts",
+    icon: "shopping_cart",
+    class: "",
   },
   {
-    path: '/products',
-    title: 'Products',
-    icon: 'library_books',
-    class: '',
+    path: "/products",
+    title: "Products",
+    icon: "shopping_bag",
+    class: "",
   },
 ];
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
